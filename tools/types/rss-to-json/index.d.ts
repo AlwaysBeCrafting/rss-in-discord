fc261c41f03b0994f22037052d618bde1f5e8cea
Mark Halls <firesetterdev@gmail.com>
2018-06-24 01:56:42 -0700

    Add typings for rss-to-json

---
+++ tools/types/rss-to-json/index.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for rss-to-json
+
+declare module "rss-to-json" {
+  export function load(
+    url: string,
+    callback: (error: string, channel: Channel) => void
+  ): void;
+
+  export interface Channel {
+    title: string;
+    description: string;
+    url: string;
+    items: Item[];
+  }
+
+  export interface Item {
+    title: string;
+    description: string;
+    link: string;
+    url: string;
+    created: number;
+    enclosures: Enclosure[];
+  }
+
+  export interface Enclosure {
+    url: string;
+    length: number;
+    type: string;
+  }
+}
