5cbcdd5629b1cde5e68d7d725e97a0cad634c1ee
Mark Halls <firesetterdev@gmail.com>
2018-07-15 02:14:28 -0700

    Replace rss-to-json with feed-to-json-promise
    resolve.mainFields prevents using module over main in package.json

---
+++ tools/types/feed-to-json-promise/index.d.ts
@@ -0,0 +1,41 @@
+declare module "feed-to-json-promise" {
+  export = Feed;
+
+  class Feed {
+    load(url: string, options?: Options): Promise<Channel>;
+    constructor(options?: Options);
+  }
+
+  interface Options {
+    count?: number;
+    timeout?: number;
+  }
+
+  interface Channel {
+    title: string;
+    description: string;
+    link: string;
+    image: Image;
+    items: Item[];
+  }
+
+  interface Image {
+    url: string;
+    title: string;
+    description: string;
+    width: number;
+    height: number;
+    type: string;
+  }
+
+  interface Item {
+    title: string;
+    description: string;
+    link: string;
+    date: string;
+    guid: string;
+    categories: string[];
+    thumbnail: string;
+    media: Image;
+  }
+}
