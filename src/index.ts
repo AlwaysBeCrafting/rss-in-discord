c597839d1f6e671794575de8c24a1cef175c7471
Mark Halls <firesetterdev@gmail.com>
2018-08-31 20:09:41 -0700

    Cleanup and typing fixes

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,10 @@ const serverID = "458034851296313375";
 
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  addFeedChannel("https://xkcd.com/rss.xml", serverID);
-  const rss = await feed.load("https://xkcd.com/rss.xml");
-  const channelId = client.channels
-    .filter(channel => channel.type === "text")
-    .find(
-      (channel: TextChannel) =>
-        channel.name === rss.title.toLowerCase().replace(/[^a-z0-9]/g, "")
-    ).id;
-  getFeedItems("https://xkcd.com/rss.xml")
-    .then(items =>
-      items.forEach(item => {
-        const channel = client.channels.get(channelId) as TextChannel;
-        channel.send(item.link);
-      })
-    )
+  const rssFeed = "https://xkcd.com/rss.xml";
+  const channelId = await addFeedChannel(rssFeed, serverID);
+  getFeedItems(rssFeed)
+    .then(items => sendFeedItems(items, channelId))
     .catch(console.error);
 });
 
@@ -35,19 +24,33 @@ client.on("message", msg => {
 client.login("NDY3ODE4MjQxNjM3NDgyNTA2.DiwNcA.2ioTOx8iFIj5KnBvMZe07lNDZVU");
 
 const addFeedChannel = async (url: string, guildID: string) => {
-  const channelName = await feed.load(url).then(feed => feed.title);
+  const channelName = await feed
+    .load(url)
+    .then(feed => feed.title.toLowerCase().replace(/[^a-z0-9]/g, ""));
   const guild = client.guilds.find(guild => guild.id === guildID);
   if (
     !guild.channels.find(
-      channel =>
-        channel.type === "text" &&
-        channel.name === channelName.toLowerCase().replace(/[^a-z0-9]/g, "")
+      channel => channel.type === "text" && channel.name === channelName
     )
   ) {
     guild.createChannel(channelName, "text");
   }
+  return client.channels
+    .filter(channel => channel.type === "text")
+    .find((channel: TextChannel) => channel.name === channelName).id;
 };
 
 const getFeedItems = async (url: string) => {
   return feed.load(url).then(rss => rss.items);
+};
+
+const sendFeedItems = async (items: Array<Feed.Item>, channelId: string) => {
+  await Promise.all(
+    items.map(item => {
+      const channel = client.channels.get(channelId) as TextChannel;
+      return channel.send(
+        item.title + ":\n<" + item.link + ">\n --------------------"
+      );
+    })
+  );
 };
--- tools/types/feed-to-json-promise/index.d.ts
+++ tools/types/feed-to-json-promise/index.d.ts
@@ -2,40 +2,42 @@ declare module "feed-to-json-promise" {
   export = Feed;
 
   class Feed {
-    load(url: string, options?: Options): Promise<Channel>;
-    constructor(options?: Options);
+    load(url: string, options?: Feed.Options): Promise<Feed.Channel>;
+    constructor(options?: Feed.Options);
   }
 
-  interface Options {
-    count?: number;
-    timeout?: number;
-  }
+  namespace Feed {
+    interface Options {
+      count?: number;
+      timeout?: number;
+    }
 
-  interface Channel {
-    title: string;
-    description: string;
-    link: string;
-    image: Image;
-    items: Item[];
-  }
+    interface Channel {
+      title: string;
+      description: string;
+      link: string;
+      image: Image;
+      items: Item[];
+    }
 
-  interface Image {
-    url: string;
-    title: string;
-    description: string;
-    width: number;
-    height: number;
-    type: string;
-  }
+    interface Image {
+      url: string;
+      title: string;
+      description: string;
+      width: number;
+      height: number;
+      type: string;
+    }
 
-  interface Item {
-    title: string;
-    description: string;
-    link: string;
-    date: string;
-    guid: string;
-    categories: string[];
-    thumbnail: string;
-    media: Image;
+    interface Item {
+      title: string;
+      description: string;
+      link: string;
+      date: string;
+      guid: string;
+      categories: string[];
+      thumbnail: string;
+      media: Image;
+    }
   }
 }
