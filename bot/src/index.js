f4cb7cf2623d06a7b5d04cd3c6f5eec03d0a9b12
Mark Halls <firesetterdev@gmail.com>
2019-08-06 03:17:14 -0700

    Add raw event listener to listen for all reactions Able to expand/collapse items, X deletes the channel

--- bot/src/index.js
+++ bot/src/index.js
@@ -17,6 +17,10 @@ const runCommand = async (args, msg) => {
 };
 
 const start = async () => {
+  const events = {
+    MESSAGE_REACTION_ADD: "messageReactionAdd"
+  };
+
   client.on("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
@@ -31,6 +35,34 @@ const start = async () => {
       const args = msg.content.split(/\s+/);
       args.shift();
       return runCommand(args, msg);
+    }
+  });
+
+  client.on("raw", async event => {
+    if (!events.hasOwnProperty(event.t) || event.d.user_id === client.user.id)
+      return;
+
+    const { user_id, message_id, emoji, channel_id, guild_id } = event.d;
+    const channel = await client.channels.get(channel_id);
+
+    if (emoji.name === "❌") {
+      channel.delete();
+    } else if (emoji.name === "↘") {
+      const message = await channel.fetchMessage(message_id);
+      const parsedMessageContent = message.content.match(/([^<]+)<([^>]+)>/);
+      const title = parsedMessageContent[1];
+      const url = parsedMessageContent[2];
+      await message.edit(`${title}${url}`);
+      await message.clearReactions();
+      await message.react("↖");
+    } else if (emoji.name === "↖") {
+      const message = await channel.fetchMessage(message_id);
+      const parsedMessageContent = message.content.match(/(.+:\s+)(\S+)/);
+      const title = parsedMessageContent[1];
+      const url = parsedMessageContent[2];
+      await message.edit(`${title}<${url}>`, { embed: null });
+      await message.clearReactions();
+      await message.react("↘");
     }
   });
 
