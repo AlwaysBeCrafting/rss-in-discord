adfdd40e87f80b0f9d982148318686a6a95e74a4
Mark Halls <firesetterdev@gmail.com>
2019-08-07 01:37:38 -0700

    Remove hard coded reference to guildID Add and Remove commands have a reference to the guild from msg

--- bot/src/commands/add.js
+++ bot/src/commands/add.js
@@ -1,38 +1,31 @@
 const { getFeed } = require("../utilities/getFeed");
+const { sendRssItems } = require("../utilities/sendRssItems");
 
-const add = async (args, guildId, client) => {
+const add = async (args, message, client) => {
   const [url] = args;
   if (!url) {
     return `Usage: @RnD add <rss URL>`;
   }
   try {
     const rssFeed = await getFeed(url);
-    const guild = await client.guilds.find(guild => guild.id === guildId);
+    const guild = message.guild;
     const channelName = rssFeed.title.toLowerCase().replace(/[^a-z0-9]/g, "");
-    const channel =
-      (await guild.channels.find(
-        ch => ch.name === channelName && ch.type === "text"
-      )) || (await guild.createChannel(channelName, "text"));
+    const channelExists = await guild.channels.find(
+      ch => ch.name === channelName && ch.type === "text"
+    );
 
+    if (channelExists) {
+      return `Channel ${url} already exists.`;
+    }
+
+    const channel = await guild.createChannel(channelName, "text");
     await sendLegend(url, channel.id, client);
-    await sendRssItems(rssFeed.items, channel.id, client);
+    await sendRssItems(rssFeed, channel.id, client);
     return `Feed Added.`;
   } catch (error) {
     console.error(error.name);
     return `Invalid rss URL.
       Usage: @RnD add <rss URL>`;
-  }
-};
-
-const sendRssItems = async (rssItems, channelId, client) => {
-  try {
-    for (const item of rssItems) {
-      const channel = await client.channels.get(channelId);
-      const message = await channel.send(`${item.title}: <${item.link}>`);
-      await message.react("↘");
-    }
-  } catch (error) {
-    console.error(error);
   }
 };
 
